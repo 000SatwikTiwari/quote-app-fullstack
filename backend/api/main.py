f4dfd6c8fe45e68f65e58e58d1a05448c36146ab
Satwik Tiwari <144311322+000SatwikTiwari@users.noreply.github.com>
2026-03-30 15:47:17 +0530

    Update main.py

--- backend/api/main.py
+++ backend/api/main.py
@@ -31,7 +31,7 @@ pwd = CryptContext(schemes=["bcrypt"], deprecated="auto")
 # CORS for frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://quote-app-fullstack-owdy.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
